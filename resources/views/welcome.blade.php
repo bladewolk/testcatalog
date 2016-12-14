d7b401952b149116572dd0c8d809e640bdf79015
Versus <bladewolk@gmail.com>
2016-12-15 01:26:06 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        .bordered {
+            border-bottom: solid;
+            border-top: solid;
+            border-left: solid;
+            border-right: solid;
+            border-color: #2e6da4;
+        }
+
+        .mgtp {
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<p class="col-sm-offset-6" style="margin-top: 50px">CATA</p>
+
+<div class="content col-md-9 col-md-offset-3">
+    <div class="col-md-1">
+        CATEGORIES
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+        <li>Fruits <input type="checkbox"></li>
+    </div>
+    <div class="col-md-6 bordered">
+        <div class="">
+            BLOCK3
+        </div>
+        <div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+        </div>
+        <div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+        </div>
+        <div>
+            <div class="col-sm-3 mgtp">
+                <img src="https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png" width="80%"
+                     height="80%">
+                Place PRICE <br>
+                <button>BY IT!</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
