49bbc1148ddc2204a3e8923398fc356e889aff6a
Versus <bladewolk@gmail.com>
2016-12-21 13:00:39 +0200

    composer to heroku

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,20 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-//Auth::routes();
 
 Route::get('/', function () {
     return redirect('/home');
 });
 Route::get('/home', 'HomeController@index');
+Route::get('/admin', 'HomeController@admin')->name('admin');
 Route::post('/loadCategory', 'HomeController@loadCategory');
 Route::post('/loadSubcategory', 'HomeController@loadSubcategory')->name('ajaxLoadSubcategories');
 Route::post('/ajaxFilter', 'HomeController@ajaxFilter')->name('ajaxFilter');
 
-
-Route::get('/admin', 'HomeController@admin')->name('admin');
 Route::post('/categories/delete', 'ajaxController@deleteCategory')->name('ajaxDelete');
 Route::post('/items/delete', 'ajaxController@deleteItem')->name('ajaxDeleteItem');
 Route::post('/categories/load', 'ajaxController@loadSubcategory')->name('subcategoryLoad');
 Route::post('items', 'ajaxController@ajaxImageDelete')->name('ajaxImageDelete');
-
 
 Route::group([
     'prefix' => 'admin',
